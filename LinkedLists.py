996b87b667be0a455ce3715d289cc98ac3622cd9
howardg <howardg@hartwick.edu>
2020-02-19 23:36:42 -0500

    Linked List

--- LinkedLists.py
+++ LinkedLists.py
@@ -1,3 +1,27 @@
 # Gavin Howard 
 # 2/19/2020
 # Linked Lists
+
+class ListNode:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+    def __repr__(self):
+        return repr(self)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = head
+
+
+    def __repr__(self):
+        nodes = []
+        curr = self.head
+        while curr:
+            nodes.append(repr(curr))
+            curr = curr.next
+        return '[' + ', '.join(nodes) + ']'
+
---
+++ driver.py
@@ -0,0 +1,6 @@
+# Gavin Howard
+# 2/19/2020
+# Driver for Singly Linked list
+
+from LinkedLists import LinkedList
+
