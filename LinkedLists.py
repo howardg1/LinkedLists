3b56af483380c993118b3bff93dc3303fe57eb40
howardg <howardg@hartwick.edu>
2020-02-25 09:25:11 -0500

    Linked List

--- LinkedLists.py
+++ LinkedLists.py
@@ -2,11 +2,11 @@
 # 2/19/2020
 # Linked Lists
 
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = next
-
 
     def __repr__(self):
         return repr(self)
@@ -16,7 +16,6 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-
     def __repr__(self):
         nodes = []
         curr = self.head
@@ -25,12 +24,10 @@ class LinkedList:
             curr = curr.next
         return '[' + ', '.join(nodes) + ']'
 
-
     def add_head(self, data):
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
-
 
     def add_end(self, data):
         new_node = Node(data)
@@ -39,12 +36,10 @@ class LinkedList:
             curr = curr.next
         curr.next = new_node
 
-
     def remove_head(self):
         new_node = self.head
         self.head = self.head.next
         return new_node.data
-
 
     def remove_end(self):
         curr_node = self.head
@@ -55,17 +50,14 @@ class LinkedList:
         prev_node.next = None
         return curr_node.data
 
-
     def clear(self):
         self.head = None
-
 
     def search_list(self, key):
         curr = self.head
         while curr and curr.data != key:
             curr = curr.next
         return curr
-
 
     def remove_node(self, key):
         curr = self.head
@@ -78,6 +70,3 @@ class LinkedList:
         elif curr:
             prev.next = curr.next
             curr.next = None
-
-
-
